080bfbf92c44ea0e58ba9e947c958b75f0d802f5
Owain Lewis <owain@owainlewis.com>
2014-12-19 21:14:36 +0000

    training data

--- src/named_entity/core.clj
+++ src/named_entity/core.clj
@@ -6,8 +6,15 @@
 ;; ****************************************************
 
 (defn tokenize
+  "Very basic tokenization based on whitespace"
   [input]
   (clojure.string/split input #"\s+"))
+
+(defn fixture
+  "Helper function that can be used to load training data
+   to test the named entity extractor"
+  [f]
+  (slurp (str "training-data/" f ".txt")))
 
 (defn extract-span
   "Extracts the information from a span"
@@ -17,16 +24,29 @@
         token-type (.getType s)]
     { :token token-type :start start :end end }))
 
-(defn make-name-finder
+(defn- make-name-finder
   "Builds a named entity classifier"
   [model]
   (with-open [model-input-stream (io/input-stream model)]
     (NameFinderME.
       (TokenNameFinderModel. model-input-stream))))
 
-(defn finder [finder-type]
-  (make-name-finder
-    (str "models/namefind/en-ner-" (name finder-type) ".bin")))
+(defn- in?
+  "Determine whether a sequence xs contains x"
+  [xs x]
+  (if (empty? xs)
+    false
+    (reduce #(or %1 %2) (map #(= %1 x) xs))))
+
+(defn- valid-finder? [f]
+  (in? [:person :date :time :location] (keyword f)))
+
+(defn- finder
+  "Utility function to build finders from existing models"
+  [finder-type]
+  (when (valid-finder? finder-type)
+    (make-name-finder
+      (str "models/namefind/en-ner-" (name finder-type) ".bin"))))
 
 ;; ****************************************************
 
@@ -55,9 +75,15 @@
    i.e (extract-entities :person \"Mr David James is a person of great taste\")"
   ([sentence]
     ((comp flatten into) []
-      (for [f [:person :date :time :location]]
-        (extract-entities f sentence))))
+      (map deref
+        (doall (map
+                 #(future (extract-entities % sentence))
+                  [:person :date :time :location])))))
   ([finder-type sentence]
    (->> sentence
         (tokenize)
         (find-entities (finder finder-type)))))
+
+;; An alias for a nicer DSL
+
+(def ?e extract-entities)
